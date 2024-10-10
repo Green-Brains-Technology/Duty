a006bfa6ebba8fe9e89c7c4e44aba4c854fa9782
frederickohe <dezynarh@gmail.com>
2024-10-10 07:03:50 +0000

    fix7:03

---
+++ Assistant/controller.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+DATA_FILE = 'app_data.json'
+
+def save_data(user_name, ai_name):
+    data = {
+        'user_name': user_name,
+        'ai_name': ai_name,
+    }
+    with open(DATA_FILE, 'w') as f:
+        json.dump(data, f)
+
+def load_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r') as f:
+            return json.load(f)
+    return None
